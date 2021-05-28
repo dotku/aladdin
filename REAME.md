db2096315173ff8a7d1819968a268d8552e8cf05
dotku <weijingjaylin@gmail.com>
2021-05-27 18:08:58 -0700

    wishlist local

---
+++ REAME.md
@@ -0,0 +1,9 @@
+# Wishlist
+
+A app to manage the wish that user would like to create. It would be used for
+seller to find the potential customer.
+
+## Todo
+
+- Add user based space, a user could have multiple list, eg wishlist/$userName/$listName,
+- Add Firebase as Server
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,150 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { useEffect, useRef, useState, RefObject } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  NativeSyntheticEvent,
+  TextInputKeyPressEventData,
+  TextInputChangeEventData,
+  FlatList,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationProp } from "@react-navigation/native";
+import { Text, View } from "../components/Themed";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+const storeData = async () => {
+  const wishList = await AsyncStorage.getItem("@wishList");
+  console.log("storeData", wishList);
+  if (!wishList) {
+    try {
+      await AsyncStorage.setItem(
+        "@wishList",
+        JSON.stringify([
+          { id: 1, title: "Car", counter: 1 },
+          { id: 2, title: "House", counter: 1 },
+          { id: 3, title: "Hapiness", counter: 1 },
+        ])
+      );
+    } catch (e) {
+      // saving error
+    }
+  }
+};
 
-export default function TabOneScreen() {
+storeData();
+
+type WishList = Array<{ id: number; title: string; counter: number }>;
+
+export default function TabOneScreen({ navigation }) {
+  const inputRef = useRef() as RefObject<TextInput>;
+  const [inputContent, setInputContent] = useState("");
+  const [wishList, setWishList] = useState<WishList>([]);
+
+  useEffect(() => {
+    genWishListFromStorage();
+
+    const unsubscribe = navigation.addListener("focus", () => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    });
+    // Return the function to unsubscribe from the event so it gets removed on unmount
+    return unsubscribe;
+  }, []);
+
+  const genWishListFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@wishList");
+      if (value !== null) {
+        // value previously stored
+        setWishList(JSON.parse(value));
+      }
+    } catch (e) {
+      // error reading value
+      console.error(e);
+    }
+  };
+
+  const genWishListUpdate = async (newWishList: WishList) => {
+    setWishList(newWishList);
+    await AsyncStorage.setItem("@wishList", JSON.stringify(newWishList));
+  };
+  const handleInputChange = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>
+  ) => {
+    setInputContent(e.nativeEvent.text);
+  };
+  const handleInputKeyPress = async (
+    e: NativeSyntheticEvent<TextInputKeyPressEventData>
+  ) => {
+    if (e.nativeEvent.key === "Enter") {
+      let newWishList = wishList;
+      const duplicatedItem = wishList.find(
+        (item) => item.title === inputContent
+      );
+      if (duplicatedItem !== undefined) {
+        newWishList = wishList.filter((item) => item.id !== duplicatedItem.id);
+      }
+      await genWishListUpdate([
+        {
+          id: duplicatedItem
+            ? duplicatedItem.id
+            : wishList[wishList.length - 1].id + 1,
+          title: inputContent,
+          counter: duplicatedItem ? duplicatedItem.counter + 1 : 1,
+        },
+        ...newWishList,
+      ]);
+      setInputContent("");
+
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <TextInput
+        value={inputContent}
+        onChange={handleInputChange}
+        onKeyPress={handleInputKeyPress}
+        placeholder="What do you require?"
+        blurOnSubmit={false}
+        style={{ padding: 2, marginTop: 30 }}
+        ref={inputRef}
+      />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <FlatList
+        style={{ maxWidth: "80%", width: 500 }}
+        data={wishList}
+        renderItem={({ index, item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text key={index.toString()}>
+              * {item.title} x {item.counter}
+            </Text>
+            <Text
+              accessibilityRole="button"
+              onPress={async () => {
+                await genWishListUpdate(
+                  wishList.filter((wishListItem) => wishListItem.id !== item.id)
+                );
+              }}
+            >
+              [x]
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
@@ -17,16 +152,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
